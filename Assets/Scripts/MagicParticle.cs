deeda81cade50030afc107ec895ad60094dc029b
Kouch-Sato <redkouchred@gmail.com>
2021-06-08 17:35:48 +0900

    ops

--- Assets/Scripts/MagicParticle.cs
+++ Assets/Scripts/MagicParticle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class MagicParticle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,9 @@ public class EnemyManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnParticleCollision(GameObject other) {
+        Debug.Log(222);
     }
 }
